9f20850623e04d7f8c09cbc2b053949aabcad1fb
Hamad NJMOLUH <wanashamad123@gmail.com>
2020-10-25 08:54:38 +0100

    ajout du commentaire

--- src/main/java/com/hmd007/hmdbootstarter/HmdBootStarterApplication.java
+++ src/main/java/com/hmd007/hmdbootstarter/HmdBootStarterApplication.java
@@ -3,6 +3,11 @@ package com.hmd007.hmdbootstarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Hamad NJIMOLUH
+ *
+ * Classe de démarrage de l'application
+ */
 @SpringBootApplication
 public class HmdBootStarterApplication {
 
